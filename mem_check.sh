f3c76aa4d916a054799f306c4fb81471ec1c973f
Vardan <vardannix@gmail.com>
2019-10-29 13:41:41 +0100

    Update mem_check.sh

--- mem_check.sh
+++ mem_check.sh
@@ -1,1 +1,1 @@
-valgrind --leak-check=full --show-reachable=yes --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt  ./test/bin/randomized_stress_test
+valgrind --leak-check=full --show-reachable=yes --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt  ./test/bin/stress_test
